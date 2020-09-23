91fb1f586492e0526bc633421492a1ecc1375c97
Somaia Abdullah <somia.abdullah3@gmail.com>
2020-09-23 01:25:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+let submitData = (a,b) => {
+  let formData = {
+  name: a,
+  email: b
+};
+  return fetch('http://localhost:3000/users',{
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  })
+  .then(response => response.json())
+  .then(obj =>{
+    let p = document.createElement("p");
+    p.innerHTML = obj.id;
+    document.body.appendChild(p);
+  })
+  .catch(error => {
+    let p = document.createElement("p");
+    p.innerHTML = error.message;
+    document.body.appendChild(p);
+  })
+}
